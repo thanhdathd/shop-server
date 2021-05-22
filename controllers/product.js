1e9c8d7d98b67271369fde5564643262d96063f6
Phi Thanh Dat <phithanhdathd@gmail.com>
2021-05-23 04:47:55 +0700

    add filter by category for product

--- controllers/product.js
+++ controllers/product.js
@@ -64,17 +64,27 @@ module.exports = {
     },
 
     list(req, res) {
-        const { page, size } = req.query;
+        const { page, size, cat } = req.query;
         const { limit, offset } = getPagination(page, size);
 
         console.log("req.params: "+req.query.page)
-        return Product
+        if (typeof(cat) != 'undefined' && cat != -1) {
+            return Product
+            .findAndCountAll({where: {categoryId: cat} ,limit, offset})
+            .then(data => {
+                const response = getPagingData(data, page, limit)
+                res.status(200).send(response);
+            })
+            .catch(error => res.status(400).send(error));
+        } else {
+            return Product
             .findAndCountAll({limit, offset})
             .then(data => {
                 const response = getPagingData(data, page, limit)
                 res.status(200).send(response);
             })
             .catch(error => res.status(400).send(error));
+        }
     },
 
     update(req, res) {
